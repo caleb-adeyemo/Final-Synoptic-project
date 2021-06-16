3add84fd129fb3e02538100027d7b45bb9cf7fff
caleb <66700303+caleb-adeyemo@users.noreply.github.com>
2021-06-16 22:21:59 +0100

    changes to the nav bar and the foooter section

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, `public`)));
 app.use(express.urlencoded({ extended: true }))
 
-// routs
-app.get("/Promotions", (req,res)=>{
-    console.log("works")
-})
+
 // listeners
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {console.log(`server listening at port ${PORT}`)});
